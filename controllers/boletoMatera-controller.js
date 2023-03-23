dfc52937d2beab82f9fc2742bc8d6646113383d5
ClaudioRaphael <106706481+ClaudioRaphael@users.noreply.github.com>
2023-03-23 17:43:04 -0300

    Atualizacao

--- controllers/boletoMatera-controller.js
+++ controllers/boletoMatera-controller.js
@@ -1,5 +1,16 @@
 const dadosBoletoMatera = require("../dados/dadosBoletoMatera");
 
 exports.postboletoMatera = (req, res) => {
+  if (req.headers["content-type"] !== "Content-Type") {
+    res.status(400).json({
+      error: [
+        {
+          code: "CAR-COB-19",
+          message:
+            "Ocorreu um erro ao chamar a API do core. Code: ORA-20010. Message: @#carteira=9@#@#cedente=1@#.",
+        },
+      ],
+    });
+  }
   res.status(200).json(dadosBoletoMatera);
 };
