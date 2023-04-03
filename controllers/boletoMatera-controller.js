33f6fa97e3c6283522b086848f0150ad92a4892b
Claudio Raphael <claudioraphaelct@gmail.com>
2023-04-03 17:01:20 -0300

    tirar validacao

--- controllers/boletoMatera-controller.js
+++ controllers/boletoMatera-controller.js
@@ -1,16 +1,5 @@
 const dadosBoletoMatera = require("../dados/dadosBoletoMatera");
 
 exports.postboletoMatera = (req, res) => {
-  if (req.headers["content-type"] !== "Topaz") {
-    res.status(400).json({
-      error: [
-        {
-          code: "CAR-COB-19",
-          message:
-            "Ocorreu um erro ao chamar a API do core. Code: ORA-20010. Message: @#carteira=9@#@#cedente=1@#.",
-        },
-      ],
-    });
-  }
   res.status(200).json(dadosBoletoMatera);
 };
